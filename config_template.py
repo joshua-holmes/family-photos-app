3977ff326c04513f8ffd1048a66ca7481ea0c144
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-05-28 12:02:11 -0500

    Change config setup

---
+++ config_template.py
@@ -0,0 +1,18 @@
+# Can add any combination of characters
+APP_KEY = b'5js?Fsjei$*2@#jf/"'
+
+# Change server, port TLS and SSL only if you are not using Gmail
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+# If using Gmail, just enter the email and password for the Google account and email will be setup
+MAIL_USERNAME = 'example@gmail.com'
+MAIL_PASSWORD = 'MyGmailPa$$word'
+
+# Create a new secret file in Google Cloud Console.
+# Save it in ./.secrets.
+# Enter the directory of the file here
+CLIENT_SECRET_FILE = "./.secrets/client_secret_700721291989-d3mldjks9s0rcp54h6limclgt3m4r5g5.apps.googleusercontent.com.json"
+
+ALBUM_ID = 'AHNRPkcjzuuMWVo83HE6X5Eq8izQOlslaNVo-s1paRRIdsQxGmteqLpuro421j0yyKzI94LEZ7r8'
