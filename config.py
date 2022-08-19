df5e7cb6603043e5faed084606075ba86d673a6e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-08-19 14:05:19 -0700

    Fix bug with sending emails

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ APP_KEY = os.getenv('FLASK_APP_KEY', '').encode()
 
 MAIL_SERVER = os.getenv('FLASK_MAIL_SERVER')
 MAIL_PORT = int(os.getenv('FLASK_MAIL_PORT', 0))
-MAIL_USE_TLS = os.getenv('FLASK_MAIL_USE_TLS')
-MAIL_USE_SSL = os.getenv('FLASK_MAIL_USE_SSL')
+MAIL_USE_TLS = os.getenv('FLASK_MAIL_USE_TLS') in ['True', 'true']
+MAIL_USE_SSL = os.getenv('FLASK_MAIL_USE_SSL') in ['True', 'true']
 MAIL_USERNAME = os.getenv('FLASK_MAIL_USERNAME')
 MAIL_PASSWORD = os.getenv('FLASK_MAIL_PASSWORD')
 
